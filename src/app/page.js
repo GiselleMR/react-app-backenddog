9e7aaf96dd0bc18d395af8572581f490d922f497
GiselleMR <giselle.rubin1@gmail.com>
2023-05-18 11:02:24 -0400

    add cors

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
+'use client';
+
 import styles from './page.module.css'
 
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+const [dogs, setDogs] = useState({});
+
+
   return (
     <main className={styles.main}>
       <h1>Hello React!</h1>
